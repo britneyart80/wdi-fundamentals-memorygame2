654d96f1f23587a26a89a8b83dd5bfa415cde386
britneyart80 <chen.bri@husky.neu.edu>
2019-05-17 20:50:39 +0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,17 @@
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne;
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+var cardTwo;
+cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again");
+	}
+}
